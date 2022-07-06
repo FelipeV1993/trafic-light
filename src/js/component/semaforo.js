9cfe41519e5e076901d5654bafcef29745176d95
felipe <f.vasquezossa@gmail.com>
2022-07-06 15:06:26 -0400

    bucle

--- src/js/component/semaforo.js
+++ src/js/component/semaforo.js
@@ -2,6 +2,31 @@ import React, { useEffect, useState } from "react";
 
 const Semaforo = () => {
 
+  function intervalo(){
+
+  }
+
+  function rojoAmarillo(){
+    
+    setColor("yellow");
+    setTimeout(amarilloVerde,1000);
+    
+  }
+  function amarilloVerde(){
+    setColor("green");
+    setTimeout(verdeRojo,1000);
+
+  }
+  function verdeRojo(){
+    setColor("red");
+    setTimeout(rojoAmarillo,1000);
+  }
+  {/* setTimeout( function() { FetchData(); }, 1000); */}
+  function myStopFunction() {
+    clearTimeout(verdeRojo);
+    clearTimeout(amarilloVerde);
+    clearTimeout(rojoAmarillo);
+  }
 
   const [color, setColor] = useState("");
 
@@ -29,6 +54,22 @@ const Semaforo = () => {
         Warning
       </button>
       <h1>{color}</h1> */}
+
+
+      {/* <button
+        type="button"
+        className="btn btn-danger"
+        onClick={() => myStopFunction()}
+      >
+        Danger
+      </button> */}
+      <button
+        type="button"
+        className="btn btn-warning"
+        onClick={() => verdeRojo()  }
+      >
+        Warning
+      </button>
 
         <div className="child">
             <div className={(color=="red") ? 'rojo-claro' : "rojo"} onClick={() => setColor("red")}>  </div>
